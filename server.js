2bd4e081f3bc196f8a4f46e0a9423933f4a9bb07
Jacob DeLeon <jacobdeleon12@gmail.com>
2019-05-20 16:25:57 -0700

    fixing file tree
    fixing file tree

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.use(express.json());
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "/public/home.html"));
+  res.sendFile(path.join(__dirname, "/app/public/home.html"));
 });
 
-app.get("/public", function(req, res) {
-  res.sendFile(path.join(__dirname, "../survey.html"));
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "/app/public/survey.html"));
 });
 
 // Starts the server to begin listening
