0a97d4c2c1e0b4698ecf18fb9fcda1cfd3f11aa1
Jacob DeLeon <jacobdeleon12@gmail.com>
2019-05-22 16:46:53 -0700

    trying to get math to work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,8 +12,28 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
-   res.json(friends[0])
    
+    var newFriend = req.body;
+    var newFriendScores = newFriend.scores;
+    var difference = 0;
+    var bestFriend = 0;
+
+    for (let i = 0; i < friends.length; i++) {
+      
+      difference = 0
+      var friendsScore = friends[i].score
+
+      for (let j = 0; j < friendsScore.length; j++) {
+        
+        difference+= Math.abs(parseInt(newFriendScores[j]) - parseInt(friends[i].scores));
+
+        if (difference <= newFriendScores) {
+          bestFriend = i
+        }
+      }
+    }
+    friends.push(newFriend);
+    res.json(friends[bestFriend]);
   });
  
  };
