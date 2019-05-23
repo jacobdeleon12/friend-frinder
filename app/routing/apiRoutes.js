f36cf2002e8f4552269b29a12f88423ec4824045
Jacob DeLeon <jacobdeleon12@gmail.com>
2019-05-22 17:07:57 -0700

    got picture to work
    got picture to work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,27 +13,28 @@ module.exports = function(app) {
 
   app.post("/api/friends", function(req, res) {
    
-    var newFriend = req.body;
-    var newFriendScores = newFriend.scores;
-    var difference = 0;
-    var bestFriend = 0;
+    // var newFriend = req.body;
+    // var newFriendScores = newFriend.scores;
+    // var difference = 0;
+    // var bestFriend = 0;
 
-    for (let i = 0; i < friends.length; i++) {
+    // for (let i = 0; i < friends.length; i++) {
       
-      difference = 0
-      var friendsScore = friends[i].score
+    //   difference = 0
+    //   var friendsScore = friends[i].score
 
-      for (let j = 0; j < friendsScore.length; j++) {
+    //   for (let j = 0; j < friendsScore.length; j++) {
         
-        difference+= Math.abs(parseInt(newFriendScores[j]) - parseInt(friends[i].scores));
+    //     difference+= Math.abs(parseInt(newFriendScores[j]) - parseInt(friends[i].scores));
 
-        if (difference <= newFriendScores) {
-          bestFriend = i
-        }
-      }
-    }
-    friends.push(newFriend);
-    res.json(friends[bestFriend]);
+    //     if (difference <= newFriendScores) {
+    //       bestFriend = i;
+    //     }
+    //   }
+    // }
+    // friends.push(newFriend);
+    // res.json(friends[bestFriend]);
+    res.json(friends[0]);
   });
  
  };
